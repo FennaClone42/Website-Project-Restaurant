d31880b8efdfdf31108c2e8e7ad3ee018c8115d3
Nickerty <43812715+Nickerty@users.noreply.github.com>
2018-10-17 11:17:09 +0200

    Add files via upload
    Link met de DB

---
+++ Datalink.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = ""; // PAS AAN ALS DE DB WEL EEN WW HEEFT
+	$dbname = "Pizza"; //PAS AAN ALS DE DB NAAM ANDERS IS
+	$db = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+	
+	if (mysqli_connect_errno()){
+		die("De verbinding met de database is mislukt" . 
+		mysqli_connect_error(). "(".
+		mysqli_connect_errno().")" );
+	} 
+	
+?>
