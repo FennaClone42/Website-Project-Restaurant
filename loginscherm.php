82ebf184ca5ac33f0612d066a5e12c95856aebe7
bob ross <fenna.steenvoorden@tribal-bing.com>
2018-10-29 12:19:54 +0100

    loginscherm
    ye

---
+++ loginscherm.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title> Login Scherm </title>
+</head>
+<body>
+<table>
+<form Action= "tba" method="POST">
+<tr>
+<td> Email:</td>
+<td><input type="email" name="email" required></td>
+</tr>
+
+<tr>
+<td> Wachtwoord:</td>
+<td><input type="password" name="wachtwoord" required></td>
+<tr>
+<td><input id="buttonlogin" type="submit" name="login" value="Log in"></td>
+
+
+</form>
+
+<form action="registatiescherm.php">
+
+<td><input id="buttonlogin" type="submit" value="registreer"></td>
+
+</form>
+</table>
+</body>
+</html>
