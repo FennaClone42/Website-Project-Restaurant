7630d061c4507695e911345dcd3c625224b91e6c
callofdeepak <44072566+callofdeepak@users.noreply.github.com>
2018-10-29 12:16:03 +0100

    Add files via upload

---
+++ bestel2.php
@@ -0,0 +1,39 @@
+<?php
+
+// session_start();
+
+  if (isset($_POST['submit'])){
+    $pizza1 = $_POST['pizza1'];
+    $pizza2 = $_POST['pizza2'];
+
+
+include 'database.php';
+// $query = "INSERT INTO orders(username, OrderID) VALUES $pizza1, $pizza2";
+$query2 = "SELECT postcode , Street_number , street , plaats FROM users;";
+$result = mysqli_query($db, $query2);
+    if (!$result) {
+        die("Database query mislukt.");
+    }
+
+    echo("De volgende artikelen zijn besteld:<br />");
+    echo("Aantal Pizza Pepperoni : $pizza1 <br>");
+    echo("Aantal Pizza Hawaii: $pizza2 <br><br>");
+    echo("<hr>");
+
+    echo("U heeft de artikelen besteld naar: <br>");
+
+while($row = mysqli_fetch_assoc($result)) {
+  echo "<td>" . $row['postcode']."</td><br>";
+  echo "<td>Huisnummer: " . $row['Street_number']."</td><br>";
+  echo "<td>" . $row['street']."</td><br>";
+  echo "<td>" . $row['plaats']."</td><br>";
+}
+
+  mysqli_close($db);
+?> <a href="bestelpagina.php"><input type="button" name="cancel" id="cancel" value="Ga terug naar bestelpagina"></a> <?php
+
+} else {
+  echo("U heeft niks ingevoerd.");
+}
+
+?>
---
+++ bestelpagina.php
@@ -0,0 +1,13 @@
+<DOCTYPE! html>
+<center><h1>Bestelpagina</h1><center>
+
+<body>
+<form method ="post" attribute="post" action="bestel2.php">
+  <fieldset>
+  <table>
+    Pizza Pepperoni:<input type="number" max="99" name="pizza1" required> <br />
+    Pizza Hawaii:<input type="number" max="99" name="pizza2" required> <p>
+  </table>
+
+    <input type = "submit" name="submit" value="Bestel">
+</form>
