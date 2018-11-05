f35c5ce5b85c57c86855ed6157a1a2fe01d42bdd
Rhadez <44072770+Rhadez@users.noreply.github.com>
2018-11-05 16:53:21 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,24 +1,29 @@
 <?php
-// session_start();
-// if (isset($_SESSION['username'])) {
-// $username = $_SESSION['username'];
-// } else {
-// echo "You have not signed in";
-// }
+session_start();
+$user = $_SESSION["Username"];
 ?>
-<h2>Weet u het zeker dat u uw profiel wilt verijwderen?</h2>
-<input type = "button" name = "profiel" value = 'Profiel Verwijderen'>
-<a href="profiel.php"><input type = "button" name = 'cancel' value = 'Annuleren' >
+<h2>Weet u het zeker dat u uw profiel wilt verwijderen?</h2>
+<form method = "post">
+<input type = "submit" name = "profiel" value = 'Profiel Verwijderen'>
+<a href="profielen.php"><input type = "button" name = 'cancel' value = 'Annuleren' >
+
 <?php
-include ('connect.php');
+include ('datalink.php');
 if (isset($_POST["profiel"])){
 
-	   // $sql= ("DELETE username, password, name, email, phone, postcode, street_number, street, plaats
-		 // FROM users WHERE username = '$username' ");
-	   // $res = mysql_query($sql);
-	   echo"Uw profiel is verwijderd.";
-		 echo "<a href='hoofdpagina.php'><input type = 'button' name = 'cancel' value = 'Terug naar Hoofdpagina' >";
-	 }
+	   $sql= "DELETE FROM users WHERE username = '$user'";
+	   $result = mysqli_query($db, $sql);
+		if (!$result) {
+		   echo "Er ging iets fout met de db verbinden";
+		   die;
+		  }
 
+			if($result){
+ 			 session_unset();
+ 				session_destroy();
+ 				header('Location: homepagina.php');
+ 					}
+				}
 
 ?>
+</form>
---
+++ editall.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$user = $_SESSION["Username"];
+?>
+<table border = 1 >
+<form method="post">
+    <tr><td colspan="2"><h3 align="center">Bewerk uw Profiel</td></tr>
+    <tr><td>Password:</td><td> <input type="text" name="password"></td></tr>
+    <tr><td>Email:</td><td> <input type="text" name="email"></td></tr>
+    <tr><td>Postcode:</td><td> <input type="text" name="postcode"></td></tr>
+    <tr><td>Street Number:</td><td> <input type="text" name="street_number"></td></tr>
+    <tr><td>Street:</td><td> <input type="text" name="street"></td></tr>
+    <tr><td>Plaats:</td><td> <input type="text" name="plaats"></td></tr>
+</table><table>
+    <td><input type="submit" name="submit" value="Verander"></td>
+    <td><a href="profielen.php"><input type = "button" name = 'cancel' value = 'Annuleren' ></td>
+    </table>
+</form>
+
+
+<?php
+  include("Datalink.php");
+  if ($db->connect_error) {
+     die("Connection failed: " . $db->connect_error);
+  }
+ if (isset($_POST["submit"])) {
+    $pass = $_POST['password'];
+    $email = $_POST['email'];
+    $pos  = $_POST['postcode'];
+    $sten  = $_POST['street_number'];
+    $ste  = $_POST['street'];
+    $plaats = $_POST['plaats'];
+
+    $query = "UPDATE `users` SET `Password` = '$pass',
+    `Email` = '$email', `Postcode` = '$pos', `Street_number` = '$sten', `Street` = '$ste',
+    `Plaats` = '$plaats' WHERE `Username` = '$user'";
+
+    $result = mysqli_query($db, $query);
+
+  if ($db->query($query) === TRUE) {
+      echo "Uw informatie is bijgewerkt.";
+      header('location: profielen.php');
+    } else {
+        echo "Error: " . $query . "<br>" . $db->error;
+    }
+$db->close();
+}
+?>
