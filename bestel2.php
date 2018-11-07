e842ca67aa92f97cc737a10b818666eb723a9674
Nickerty <43812715+Nickerty@users.noreply.github.com>
2018-11-08 00:33:34 +0100

    Update bestel2.php

--- bestel2.php
+++ bestel2.php
@@ -1,40 +1,48 @@
 <?php
 
 session_start();
-$user = $_SESSION["Username"];
+if (!isset($_SESSION["Username"])) 
+{
+	echo "U bent niet ingelogd";
+	echo "<br>";
+	echo "<a href='Login.php'>Inloggen</a>";
+} 
+else 
+{
+	$user = $_SESSION["Username"];
 
-  if (isset($_POST['submit'])){
-    $pizza1 = $_POST['pizza1'];
-    $pizza2 = $_POST['pizza2'];
+	if (isset($_POST['submit']))
+	{
+		$pizza1 = $_POST['pizza1'];
+		$pizza2 = $_POST['pizza2'];
 
 
-include 'database.php';
-// $query = "INSERT INTO orders(username, OrderID) VALUES $pizza1, $pizza2";
-$query2 = "SELECT `postcode` , `Street_number` , `street` , `plaats` FROM `users` WHERE `username` = '$user';";
-$result = mysqli_query($db, $query2);
-    if (!$result) {
-        die("Database query mislukt.");
-    }
+	include 'datalink.php';
+	$query = "SELECT `postcode` , `Street_number` , `street` , `plaats` FROM `users` WHERE `username` = '$user';";
+	$result = mysqli_query($db, $query);
+		if (!$result) {
+			die("Database query mislukt.");
+		}
 
-    echo("De volgende artikelen zijn besteld:<br />");
-    echo("Aantal Pizza Pepperoni : $pizza1 <br>");
-    echo("Aantal Pizza Hawaii: $pizza2 <br><br>");
-    echo("<hr>");
+		echo("De volgende artikelen zijn besteld:<br />");
+		echo("Aantal Pizza Pepperoni : $pizza1 <br>");
+		echo("Aantal Pizza Hawaii: $pizza2 <br><br>");
+		echo("<hr>");
 
-    echo("U heeft de artikelen besteld naar: <br>");
+		echo("U heeft de artikelen besteld naar: <br>");
 
-while($row = mysqli_fetch_assoc($result)) {
-  echo "<td>" . $row['postcode']."</td><br>";
-  echo "<td>Huisnummer: " . $row['Street_number']."</td><br>";
-  echo "<td>" . $row['street']."</td><br>";
-  echo "<td>" . $row['plaats']."</td><br>";
-}
+	while($row = mysqli_fetch_assoc($result)) {
+	  echo "<td>" .$row['street'].' '. $row['Street_number']."</td><br>";
+	  echo "<td>" .$row['postcode']."</td><br>";
+	  echo "<td>" .$row['plaats']."</td><br>";
+	}
 
-  mysqli_close($db);
-?> <a href="bestelpagina.php"><input type="button" name="cancel" id="cancel" value="Ga terug naar bestelpagina"></a> <?php
+	  mysqli_close($db);
+	?> <a href="bestelpagina.php"><input type="button" name="cancel" id="cancel" value="Ga terug naar bestelpagina"></a> <?php
 
-} else {
-  echo("U heeft niks ingevoerd.");
+	} else {
+	  echo("U heeft niks ingevoerd.");
+	}
 }
 
 ?>
