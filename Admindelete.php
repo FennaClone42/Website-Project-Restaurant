a8bd84d78241b5e8087bdde6b85c9598a931faf4
Nickerty <43812715+Nickerty@users.noreply.github.com>
2018-11-08 00:31:10 +0100

    Update Admindelete.php

--- Admindelete.php
+++ Admindelete.php
@@ -1,37 +1,39 @@
 <form  method="post">
 <?php
-<<<<<<< HEAD
-include 'Datalink.php';
-$gebruikersnaam = $_GET['user'];
-echo "Gebruiker $gebruikersnaam verwijderen, weet u het zeker?";
- $sql= "DELETE FROM `users` WHERE `Username` = '$gebruikersnaam'";
-echo "<input type='submit' name='delete' value='Ja'>";
-echo "</form><form action='admin.php'>";
-echo "<input type='submit' name='cancel' value='Nee'>";
- if(isset($_POST['delete'])){
-   $result = mysqli_query($db, $sql);
-  if (!$result) {
-     echo "Er ging iets fout met de db verbinden";
-     die;
-    }
- }
-=======
+session_start();
+if (!isset($_SESSION["Admin"])) {
+	echo "U bent niet ingelogd";
+	echo "<br>";
+	echo "<a href='Login.php'>Inloggen</a>";
+}
+$admin = $_SESSION["Admin"];
+if ($admin != 'admin') {
+	echo "U hebt geen admin rechten";
+	echo "<br>";
+	echo "<a href='Bestelpagina.php'>Terug naar de bestelpagina</a>";
+} else {
+	
 	include 'Datalink.php';
 	$gebruikersnaam = $_GET['user'];
 	echo "Gebruiker $gebruikersnaam verwijderen, weet u het zeker?";
 	echo "<input type='submit' name='delete' value='Ja'>";
-	echo "</form><form action='admin.php'>";
+	echo "</form><form action='admingebruikers.php'>";
 	echo "<input type='submit' name='cancel' value='Nee'>";
 	if(isset($_POST['delete']))
 	{
 		$sql= "DELETE FROM users WHERE Username = '$gebruikersnaam'";
+		echo $sql;
 		$result = mysqli_query($db, $sql);
 		if (!$result) 
 		{
-			echo "Er ging iets fout met de db verbinden";
+			echo "Er ging iets fout met met de db verbinden";
 			die;
 		}
+		else
+		{
+			header("location:admingebruiker.php");
+		}
 	 }
->>>>>>> 3a075e2a72e85cb5bd304661e588dfa30f123c05
+}
 ?>
 </form>
