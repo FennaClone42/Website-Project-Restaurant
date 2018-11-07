ce9502bb36f3ea5a0cce0a2b34941571c04748c0
Nickerty <43812715+Nickerty@users.noreply.github.com>
2018-11-08 00:32:43 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,29 +3,43 @@
 <head>
 	<?php
 session_start();
-$admin = $_SESSION["Admin"];
-if ($admin != 'admin') {
-	echo "Je hebt geen admin rechten";
-} else {
+if (!isset($_SESSION["Admin"])) 
+{
+	echo "U bent niet ingelogd";
+	echo "<br>";
+	echo "<a href='Login.php'>Inloggen</a>";
+}
+else 
+{
+	$admin = $_SESSION["Admin"];
+	if ($admin != 'admin') 
+	{
+		echo "U hebt geen admin rechten";
+		echo "<br>";
+		echo "<a href='Bestelpagina.php'>Terug naar de bestelpagina</a>";
+	} 
+
+	else 
+{
 	 ?>
 
-	<style> /*Hiermee kan je div mee bewerken soort css*/
+	<style> <!--Hiermee kan je div mee bewerken soort css-->
   .btn-group .button {
-        background-color: #008CBA; /*Achtergrond kleur van button*/
-        border: 1px solid blue; /*Breedte, stijl en kleur van de rand van een element opgeven*/
-        color: white; /*Tekst kleur in de button*/
-        padding: 15px 32px; /*Opvullingseigenschap boven- en ondervulling zijn 15px en de linker- en rechterpadding zijn 5 px*/
-        text-align: center; /*Positie tekst in midden van de button*/
-        display: inline-block; /*Geen regelovergang na het element, zodat het element naast andere elementen kan zitten.*/
-        font-size: 16px; /*Tekengrootte*/
-        cursor: pointer; /*Hiermee heeft de muissymbool een handje als vorm*/
-        float: center; /*Positie buttons in  het midden van de pagina*/
+        background-color: #008CBA; <!--Achtergrond kleur van button-->
+        border: 1px solid blue; <!--Breedte, stijl en kleur van de rand van een element opgeven-->
+        color: white; /*Tekst kleur in de button-->
+        padding: 15px 32px; <!--Opvullingseigenschap boven- en ondervulling zijn 15px en de linker- en rechterpadding zijn 5 px-->
+        text-align: center; <!--Positie tekst in midden van de button-->
+        display: inline-block; <!--Geen regelovergang na het element, zodat het element naast andere elementen kan zitten.-->
+        font-size: 16px; <!--Tekengrootte-->
+        cursor: pointer; <!--Hiermee heeft de muissymbool een handje als vorm-->
+        float: center; <!--Positie buttons in  het midden van de pagina-->
     }
     .btn-group .button:not(:last-child) {
-        border-right: none; /* Hiermee voorkom je dubbele borders*/
+        border-right: none; <!-- Hiermee voorkom je dubbele borders-->
     }
     .btn-group .button:hover {
-        background-color: #555555; /*Indien je met de muis over de knop beweegt wordt deze de kleur grijs*/
+        background-color: #555555; <!--Indien je met de muis over de knop beweegt wordt deze de kleur grijs-->
     }
 		</style>
 </head>
@@ -35,12 +49,12 @@ if ($admin != 'admin') {
 	<div class="btn-group">
 	<!-- Het aanmaken van een button groep die naast elkaar wordt weergegeven -->
 <tr>
-<input type="button" class= "button" onclick="location.href='soorten.php';" value="Pizzalijst" />
+<input type="button" class= "button" onclick="location.href='adminsoorten.php';" value="Pizzalijst" />
 <!-- Met deze knop wordt er informatie uit de database gehaald (Pizzalijst) -->
-<input type="button" class= "button" onclick="location.href='gebruikers.php';" value="Gebruikergegevens" />
+<input type="button" class= "button" onclick="location.href='Admingebruikers.php';" value="Gebruikergegevens" />
 <!-- Met deze knop wordt er informatie uit de database gehaald (Gebruikergegevens)-->
 </tr>
-<?php } ?>
+<?php }} ?>
 </div>
 </body>
 
